3faf40b37daa257d83138f4d6a70a03fc86293a0
theMaximmus <m.stesev@gmail.com>
2025-05-10 14:38:33 -0700

    Last stable version. (rolled back to FXML usage) Cleaned up the code. Implemented the logic to filter the data (does not use Collections)

--- src/main/java/csm/cis255/cis255_projectfour/DistrictAreaController.java
+++ src/main/java/csm/cis255/cis255_projectfour/DistrictAreaController.java
@@ -9,6 +9,7 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 
 import static csm.cis255.cis255_projectfour.DistrictArea.districtAreaList;
+import static csm.cis255.cis255_projectfour.DistrictArea.filteredDistrictAreaList;
 
 public class DistrictAreaController {
     private static DistrictAreaController instance;
@@ -48,13 +49,46 @@ public class DistrictAreaController {
 //    static ObservableList<String> districtGradeLevelList = grade
     @FXML
     protected void onSearchButtonClick() {
-        DistrictArea.fillListAndMap();
-        for (DistrictArea districtArea : districtAreaList) {
-            textFlow.getChildren().add(new Text(districtArea.toString()));
-            Separator horizontalSeparator = new Separator(Orientation.HORIZONTAL);
-            horizontalSeparator.setMinWidth(textFlow.getWidth());
-            textFlow.getChildren().add(horizontalSeparator);
-            textFlow.getChildren().add(new Text("\n"));
+        // Obtain drop-down menu (ComboBox) values
+        String countyName = isNeitherNullOrEmpty(countyNameComboBox.getSelectionModel().getSelectedItem().toString()) ? countyNameComboBox.getSelectionModel().getSelectedItem().toString() : null;
+        String geographicalLocale = isNeitherNullOrEmpty(geographicalLocaleComboBox.getSelectionModel().getSelectedItem().toString()) ? geographicalLocaleComboBox.getSelectionModel().getSelectedItem().toString() : null;
+        String gradeLevel = isNeitherNullOrEmpty(gradeLevelComboBox.getSelectionModel().getSelectedItem().toString()) ? gradeLevelComboBox.getSelectionModel().getSelectedItem().toString() : null;
+
+        if (isNeitherNullOrEmpty(countyName) && isNeitherNullOrEmpty(geographicalLocale) && isNeitherNullOrEmpty(gradeLevel)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByCountyGradeLocale(countyName, geographicalLocale, gradeLevel);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(countyName) && isNeitherNullOrEmpty(geographicalLocale)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByCountyLocale(countyName, geographicalLocale);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(countyName) && isNeitherNullOrEmpty(gradeLevel)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByCountyGrade(countyName, gradeLevel);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(geographicalLocale) && isNeitherNullOrEmpty(gradeLevel)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByGradeLocale(geographicalLocale, gradeLevel);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(countyName)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByCounty(countyName);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(geographicalLocale)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByLocale(geographicalLocale);
+            // Populate the filtered data
+            populateFilteredData();
+        } else if (isNeitherNullOrEmpty(gradeLevel)) {
+            // Search and filter the data
+            DistrictArea.searchAndListDataByGrade(gradeLevel);
+            // Populate the filtered data
+            populateFilteredData();
         }
     }
 
@@ -74,16 +108,29 @@ public class DistrictAreaController {
     }
 
     /**
-     * Clears the Text Flow view from any displayed notes.
+     * Clears the TextFlow view from any displayed notes. Also deselects any ComboBox values and clears filtered list.
      */
     @FXML
     protected void onResetButtonClick() {
+        // Clear TextFlow
         textFlow.getChildren().clear();
+
+        // Clear each drop-down menu (ComboBox) and return the Prompt Text
+        countyNameComboBox.getSelectionModel().clearSelection();
+        countyNameComboBox.setPromptText("County Name");
+
+        geographicalLocaleComboBox.getSelectionModel().clearSelection();
+        geographicalLocaleComboBox.setPromptText("Geographical Locale");
+
+        gradeLevelComboBox.getSelectionModel().clearSelection();
+        gradeLevelComboBox.setPromptText("Grade Level");
+
+        // Clear the filtered list (filteredDistrictAreaList)
+        filteredDistrictAreaList.clear();
+
+        // Print a message in the console
         System.out.println("Successfully cleared the TextFlow view");
     }
-
-
-
 
     /**
      * Helper method that adds GeographicalLocale enum to the ComboBox items list.
@@ -107,5 +154,24 @@ public class DistrictAreaController {
      */
     public void addItemToCountyNameComboBox(String item) {
         countyNameComboBox.getItems().add(item);
+    }
+
+    /**
+     * Helper method that checks the passed values obtained from the drop-down menu (ComboBox).
+     * @param string Obtained value..
+     * @return Returns false if the parameter is either empty or null..
+     */
+    private static boolean isNeitherNullOrEmpty(String string) {
+        return string != null && !string.isEmpty();
+    }
+
+    private void populateFilteredData() {
+        for (DistrictArea districtArea : filteredDistrictAreaList) {
+            textFlow.getChildren().add(new Text(districtArea.toString()));
+            Separator horizontalSeparator = new Separator(Orientation.HORIZONTAL);
+            horizontalSeparator.setMinWidth(textFlow.getWidth());
+            textFlow.getChildren().add(horizontalSeparator);
+            textFlow.getChildren().add(new Text("\n"));
+        }
     }
 }
